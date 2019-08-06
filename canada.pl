79ec67177573303d7808d4588529057b6cbfd2d5
Eduardo Andres-Leon <eduardo.andres.leon@gmail.com>
2019-08-06 11:21:37 +0200

    probando

--- canada.pl
+++ canada.pl
@@ -7,15 +7,15 @@ my %patient;
 my %geneid;
 
 while(my $linea=<IN>){
-      chomp($linea);
-              if($linea =~/(^DO\w+)\t.+\t.+\t.+\t.+\t.+\t(.+)\t.+\t(.+)\tGRCh37/) {
-                        my $id=$1;
-                        my $gene=$2;
-                        my $raw=$3;
-                        $geneid{$gene}=$raw;
-                        $patient{$id}=$raw;
-                        $hash{$gene}{$id}=$raw;
-                        }
+	chomp($linea);
+	if($linea =~/(^DO\w+)\t.+\t.+\t.+\t.+\t.+\t(.+)\t.+\t(.+)\tGRCh37/) {
+		my $id=$1;
+		my $gene=$2;
+		my $raw=$3;
+		$geneid{$gene}=$raw;
+		$patient{$id}=$raw;
+		$hash{$gene}{$id}=$raw;
+	}
 }
 close (IN);
 

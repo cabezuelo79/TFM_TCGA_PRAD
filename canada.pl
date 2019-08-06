99ed654aa880c1f2e1946f37e3ade19475ec0ad3
Eduardo Andres-Leon <eduardo.andres.leon@gmail.com>
2019-08-06 11:45:35 +0200

    few chagens to process prueba.tsv properly

--- canada.pl
+++ canada.pl
@@ -2,34 +2,58 @@
 use Data::Dumper;
 open (IN, "prueba.tsv");
 
+use strict;
+
 my %hash;
 my %patient;
 my %geneid;
 
-while(my $linea=<IN>){
-	chomp($linea);
-	if($linea =~/(^DO\w+)\t.+\t.+\t.+\t.+\t.+\t(.+)\t.+\t(.+)\tGRCh37/) {
-		my $id=$1;
-		my $gene=$2;
-		my $raw=$3;
-		$geneid{$gene}=$raw;
-		$patient{$id}=$raw;
-		$hash{$gene}{$id}=$raw;
+while(<IN>){
+	chomp;
+	if($_ !~/^icgc_donor_id/){
+		#Esto seria más facil con un split de tabuladores porque el fichero es tsv
+		my @data=split(/\t/);
+		$geneid{$data[7]}++;
+		$patient{$data[0]}++;
+		$hash{$data[7]}{$data[0]}=$data[9];
 	}
+		
 }
 close (IN);
 
-foreach $id (sort keys %patient) {
-               print $id."\t";
-}
-print "\n";
+####### Lo más fácil #####################################
+#foreach $id (sort {$a cmp $b} keys %patient) {
+#	print $id."\t";
+#}
+#print "\n";
+#
+#foreach my $genename (sort {$a cmp $b} keys %geneid) {
+#	print $genename."\t"; 
+#	foreach my $paciente_id (sort {$a cmp $b} keys %patient) {
+#		print $hash{$genename}{$paciente_id} ."\t";
+#	}
+#	print "\n";
+#}
+#########################################################
 
-foreach $gene (keys %geneid) {
-                print $gene."\t"; 
-                       foreach $id (sort keys %patient) {
-                               foreach ($hash{$gene}{$id}) {
-                                          print $raw;
-                                          print "\n";
-                                   }
-                  }
+
+#Lo mas correcto (lo anterior incluye un tab al final)###
+
+#Cabeceras unidas por un tab. Incluyo un tab al principio para q se vean bien los genes (creo que a R esto no le gusta asi
+# que despues de comprobar que esta bien, ese tab habria que quitarlo)
+
+print "GeneNames\t". join("\t",sort {$a cmp $b} keys %patient ) ."\n";
+#Recorro los genes
+foreach my $genename (sort {$a cmp $b} keys %geneid) {
+	#Lo imprimo
+	print $genename."\t";
+	my @values;
+	foreach my $paciente_id (sort {$a cmp $b} keys %patient) {
+		#almaceno los valores en un array (que es ordenado) y los imprimo debajo con un join
+		push(@values,$hash{$genename}{$paciente_id});
+	}
+	print STDERR scalar(@values) ."\n";
+	print join("\t",@values) ."\n";
 }
+
+#########################################################
